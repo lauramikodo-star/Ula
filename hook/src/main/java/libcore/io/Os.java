c6493d09ec9755aa28d9157a15437a987c738cad
lauramikodo-star <lauramikodo@gmail.com>
2025-12-28 05:46:44 +0100

    Add hook/src/main/java/libcore/io/Os.java

---
+++ hook/src/main/java/libcore/io/Os.java
@@ -0,0 +1,9 @@
+package libcore.io;
+
+import java.io.FileDescriptor;
+
+public interface Os {
+    // Interface definition with common OS methods
+    // void close(FileDescriptor fd) throws ErrnoException;
+    // ...
+}
